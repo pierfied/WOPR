bebdb99f1a27a5ea1dea2cb7a0653d8570d1e0da
Pier Fiedorowicz <pierfied@email.arizona.edu>
2020-01-19 19:07:26 -0700

    Added MessageInterface class for passing messages over a socket.

---
+++ WOPR/message_interface.py
@@ -0,0 +1,30 @@
+import struct
+
+
+class MessageInterface:
+    def __init__(self, socket):
+        self.socket = socket
+
+    def send_msg(self, msg):
+        # Prefix each message with a 4-byte length (network byte order)
+        msg = struct.pack('>I', len(msg)) + msg
+        self.socket.sendall(msg)
+
+    def recv_msg(self):
+        # Read message length and unpack it into an integer
+        raw_msglen = self.recvall(4)
+        if not raw_msglen:
+            return None
+        msglen = struct.unpack('>I', raw_msglen)[0]
+        # Read the message data
+        return self.recvall(msglen)
+
+    def recvall(self, n):
+        # Helper function to recv n bytes or return None if EOF is hit
+        data = bytearray()
+        while len(data) < n:
+            packet = self.socket.recv(n - len(data))
+            if not packet:
+                return None
+            data.extend(packet)
+        return data
