14a2ebfc8a04d97e0f510fe4e3e847c8ed9edca1
Pier Fiedorowicz <pierfied@email.arizona.edu>
2020-01-21 06:39:12 -0700

    Added locks for sending and receiving data using the message interface.

--- WOPR/message_interface.py
+++ WOPR/message_interface.py
@@ -1,23 +1,28 @@
 import struct
+import threading
 
 
 class MessageInterface:
     def __init__(self, socket):
         self.socket = socket
+        self.send_lock = threading.Lock()
+        self.recv_lock = threading.Lock()
 
     def send_msg(self, msg):
-        # Prefix each message with a 4-byte length (network byte order)
-        msg = struct.pack('>I', len(msg)) + msg
-        self.socket.sendall(msg)
+        with self.send_lock:
+            # Prefix each message with a 4-byte length (network byte order)
+            msg = struct.pack('>I', len(msg)) + msg
+            self.socket.sendall(msg)
 
     def recv_msg(self):
-        # Read message length and unpack it into an integer
-        raw_msglen = self.recvall(4)
-        if not raw_msglen:
-            return None
-        msglen = struct.unpack('>I', raw_msglen)[0]
-        # Read the message data
-        return self.recvall(msglen)
+        with self.recv_lock:
+            # Read message length and unpack it into an integer
+            raw_msglen = self.recvall(4)
+            if not raw_msglen:
+                return None
+            msglen = struct.unpack('>I', raw_msglen)[0]
+            # Read the message data
+            return self.recvall(msglen)
 
     def recvall(self, n):
         # Helper function to recv n bytes or return None if EOF is hit
